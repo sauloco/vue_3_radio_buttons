286245aa705357051d1de135ecc27c09f5db9b68
Saulo Vargas <sauloco022@gmail.com>
2023-03-29 17:03:57 -0300

    implemented RadioAccordion using tailwind and typescript

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,73 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container mx-auto py-8">
+    <form>
+      <h2 class="text-gray-400">What are you looking to do?</h2>
+      <RadioAccordion
+        :modelValue="modelValue"
+        :name="name"
+        :options="options"
+        @update:modelValue="updateModelValue"
+      >
+    </RadioAccordion>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    </form>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script lang="ts">
+import type Option from 'types/Option';
+import { defineComponent, ref } from 'vue';
+import RadioAccordion from './components/RadioAccordion.vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+export default defineComponent({
+  name: 'App',
+  components: {
+    RadioAccordion,
+  },
+  setup() {
+    const modelValue = ref<number>(0);
+    const name = 'options';
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+    const options: Option[] = [
+      {
+        id: 'option1',
+        title: 'Option 1',
+        subtitle: 'Subtitle of option 1',
+        value: 1,
+        details: [{text: 'test link', target: 'https://example.com'}]
+      },
+      {
+        id: 'option2',
+        title: 'Option 2',
+        subtitle: 'Subtitle of option 2',
+        value: 2,
+      },
+      {
+        id: 'option3',
+        title: 'Option 3',
+        subtitle: 'Subtitle of option 3',
+        value: 3,
+      },
+    ];
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+    const updateModelValue = (value: Number) => {
+      modelValue.value = value;
+    };
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+    return {
+      modelValue,
+      name,
+      options,
+      updateModelValue,
+    };
+  },
+});
+</script>
+
+<style lang="postcss">
+/* Import Tailwind CSS */
+@import "tailwindcss/base";
+@import "tailwindcss/components";
+@import "tailwindcss/utilities";
 </style>
+
